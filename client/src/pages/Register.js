ed3b5b737fb5225d16c1be654b68833e7423027c
amahh014 <nizammamedovv@gmail.com>
2020-01-03 19:37:07 +0400

    Registration form + css

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Button,Form } from 'semantic-ui-react';
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
 function Register  ()  {
     const [values, setValues] = useState({
@@ -7,10 +9,27 @@ function Register  ()  {
         email: '',
         password: '',
         confirmPassword: ''
-
     })
+
+    const onChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value});
+    }
+
+
+    const [addUser, { loading }] = useMutation(REGISTER_USER, {
+        update(proxy, result){
+          console.log(result)
+        },
+        variables: values
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addUser();
+    }
+
     return (
-        <div>
+        <div className="form-container">
             <Form onSubmit={onSubmit} noValidate>
               <h1>Register</h1>
               <Form.Input
@@ -48,5 +67,25 @@ function Register  ()  {
         </div>
     );
 }
+
+const REGISTER_USER = gql`
+  mutation register(
+      $username: String!
+      $email: String!
+      $password: String!
+      $confirmPassword: String!
+  ) {
+      register (
+          registerInput: {
+              username: $username
+              email: $email
+              password: $password
+              confirmPassword: $confirmPassword
+          }
+      ) {
+          id email username createdAt token
+      }
+  }
+`
 
 export default Register;
